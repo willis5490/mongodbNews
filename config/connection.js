ff4b6dfabd3faa029b31b8e10b746a405d5d8dc0
William Stearns <wcstearns@yahoo.com>
2018-09-30 12:39:00 -0600

    goodies

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,23 @@
+import { SSL_OP_NO_TICKET } from "constants";
+
 const mongoose = require("mongoose");
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+export const MONGODB = "mongodb://localhost/mongoHeadlines";
+
+if(process.env.MONGODB_URI){
+    mongoose.connect(process.env.MONGODB_URI)
+}else{
+    mongoose.connect(MONGODB)
+}
+
  mongoose.Promise = Promise;
  mongoose.connect(MONGODB_URI);
+
+ const db = mongoose.connection
+
+ db.on('error', (err)=>{
+     console.log(`mongoose error: ${err}`)
+ })
+ db.once('open', ()=>{
+     console.log(`mongoose connection successful!`)
+ })
